d5df63f62716e0da9380e7b31933b5e764299e6b
Daniel Downes <danieldownescentennial@gmail.com>
2020-08-06 11:33:33 -0700

    Re-added movement, but forward and backwards movement is stuck to the Z axis rather than a local Z axis. Looking for fix now.

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
 import pyglet
+from pyglet.window import key
 import ratcave as rc
 
 window = pyglet.window.Window()
+keys = key.KeyStateHandler()
+window.push_handlers(keys)
 
 
 def update(dt):
@@ -17,6 +20,30 @@ monkey = obj_reader.get_mesh("Monkey")
 monkey.position.xyz = 0, 0, -3
 
 scene = rc.Scene(meshes=[monkey])
+
+
+def rotate_mesh(dt):
+    monkey.rotation.y += 15 * dt
+
+
+pyglet.clock.schedule(rotate_mesh)
+
+
+def move_camera(dt):
+    camera_move_spd = 5
+    camera_turn_spd = 100
+    if keys[key.LEFT]:
+        scene.camera.rotation.y += camera_turn_spd * dt
+    if keys[key.RIGHT]:
+        scene.camera.rotation.y -= camera_turn_spd * dt
+
+    if keys[key.UP]:
+        scene.camera.position.z -= camera_move_spd * dt
+    if keys[key.DOWN]:
+        scene.camera.position.z += camera_move_spd * dt
+
+
+pyglet.clock.schedule(move_camera)
 
 
 @window.event
